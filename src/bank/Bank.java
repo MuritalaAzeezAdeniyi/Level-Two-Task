c3c7d347dd3e67c4ec98dc4f14496d00ca53e445
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2025-04-11 12:36:43 +0100

    added withdraw  function

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -18,5 +18,16 @@ public class Bank {
             System.out.println(this.name + " can't deposit $0.");
         }
     }
-
+      public void withdraw(double amount) {
+        if(amount <= 0){
+            System.out.println(this.name + " can't withdraw positive amount");
+        }
+        else if(amount > this.balance){
+            System.out.println("Insufficient balance");
+        }
+        else {
+            this.balance -= amount;
+            System.out.println("Withdraw $" + amount);
+        }
+      }
 }
