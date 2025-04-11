f924eead879db884a3c6f6e7bd6982aafcba7799
Abdulazeez <azeezmuritalaadeniyi@gmail.com>
2025-04-11 12:25:54 +0100

    added deposit function

---
+++ src/bank/Bank.java
@@ -0,0 +1,22 @@
+package bank;
+
+public class Bank {
+    private String name;
+    private double balance;
+
+    public Bank(String name) {
+        this.name = name;
+        this.balance = 0.0;
+
+    }
+    public void deposit(double amount) {
+        if(amount > 0){
+            this.balance += amount;
+            System.out.println(this.name + " deposit $" + amount+"and balance $" + this.balance);
+        }
+        else {
+            System.out.println(this.name + " can't deposit $0.");
+        }
+    }
+
+}
